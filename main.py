c52768638b1f06d3eede7142cd973b90cfb6f5b2
Dytia <69058656+Dytia@users.noreply.github.com>
2022-11-09 12:20:52 +0000

    added basic properties

--- main.py
+++ main.py
@@ -2,17 +2,39 @@ import time
 import os
 import json
 
+
 a = 1
 jsonFileLocation = "./config/positions.json"
 
 class Level:
+    # impassable = no value
+    '''
+    "sample room":{
+        "up": "room1",
+        "down":"",
+        "left":"room2",
+        "right":"",
+        "properties":"item",
+        "data":"you see",
+    }
+
+    blank template:
+    "":{
+        "up": "",
+        "down":"",
+        "left":"",
+        "right":"",
+        "properties":"",
+        "data":""
+    },    
+    '''
     def __init__(self, jsonLocation):
         self._jsonLocation = jsonLocation
 
     def read_json(self):
         with open(self._jsonLocation, "r") as f:
             data = json.load(f)
-            print(data)
+            print(data["Start"])
             #data["Start"] for starting area
         return data
 
@@ -28,6 +50,10 @@ def user_input(situation):
 
 print("Hello, welcome to the game. Press Enter key to continue")
 input()
-os.system("CLS")
+a = Level(jsonFileLocation)
+data = a.read_json()
+print(data["Start"]["startingRoom"])
+#os.system("CLS")
 print("a")
+
 input()
